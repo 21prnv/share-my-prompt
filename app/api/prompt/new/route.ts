da6481080c2c4b176607dcb74a3a984fa1639edf
21prnv <pranavpatil6251@gmail.com>
2024-07-30 20:23:16 +0530

    post error

--- app/api/prompt/new/route.ts
+++ app/api/prompt/new/route.ts
@@ -1,33 +1,18 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
-import { Profile } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
 
-interface GoogleProfile extends Profile {
-  picture?: string;
-}
-export const POST = async (request: {
-  json: () =>
-    | PromiseLike<{
-        userId: String;
-        prompt: String;
-        tag: String;
-        image: String;
-      }>
-    | { userId: String; prompt: String; tag: String; image: String };
-}) => {
-  const { userId, prompt, tag, image } = await request.json();
-  console.log(userId, prompt, tag);
-
+export const POST = async (request: NextRequest) => {
   try {
+    const { userId, prompt, tag, image } = await request.json();
+
     await connectToDB();
     const newPrompt = new Prompt({ creator: userId, prompt, tag, image });
-    console.log(userId, prompt, tag);
 
     await newPrompt.save();
-    return new Response(JSON.stringify(newPrompt), { status: 201 });
+    return new NextResponse(JSON.stringify(newPrompt), { status: 201 });
   } catch (error) {
     console.log(error);
-
-    return new Response("Failed to create a new prompt", { status: 500 });
+    return new NextResponse("Failed to create a new prompt", { status: 500 });
   }
 };
